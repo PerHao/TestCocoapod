266c81edd49b753fe434ada32b5e4efc7f87ebde
per <260115678@qq.com>
2015-04-24 23:39:49 +0800

    新建项目
    初始化测试

---
+++ Podfile
@@ -0,0 +1,6 @@
+platform :ios, '7.0'
+pod 'WebViewJavascriptBridge', '~> 4.1.4'
+pod 'AFNetworking', '~> 2.3.1'
+pod 'Masonry', '~> 0.6.1'
+pod 'TPKeyboardAvoiding', '~> 1.2.4'
+source 'https://github.com/CocoaPods/Specs.git'
--- TestCocoapod/ViewController.m
+++ TestCocoapod/ViewController.m
@@ -7,8 +7,11 @@
 //
 
 #import "ViewController.h"
+#import "pinyin.h"
+//#import "WebViewJavascriptBridge.h"
 
 @interface ViewController ()
+//@property (strong, nonatomic) IBOutlet UIWebView *myWebView;
 
 @end
 
@@ -17,6 +20,46 @@
 - (void)viewDidLoad {
     [super viewDidLoad];
     // Do any additional setup after loading the view, typically from a nib.
+    
+//    [self.myWebView loadRequest:[NSURLRequest requestWithURL:[NSURL URLWithString:@"http://www.baidu.com"]]];
+   self.myWebView.backgroundColor = [UIColor whiteColor];
+    self.view.backgroundColor = [UIColor whiteColor];
+    NSString *path = [[NSBundle mainBundle] pathForResource:@"kuahangzhuanzhangxieyi" ofType:@"htm"];
+    NSURL *url = [NSURL fileURLWithPath:path];
+    NSURLRequest *request = [NSURLRequest requestWithURL:url];
+    [self.myWebView loadRequest:request];
+    
+    
+    
+//    [self.view addSubview:self.myWebView];    NSString *hanziText = @"我是中国人";
+//    if ([hanziText length]) {
+//        NSMutableString *ms = [[NSMutableString alloc] initWithString:hanziText];
+//        if (CFStringTransform((__bridge CFMutableStringRef)ms, 0, kCFStringTransformMandarinLatin, NO)) {
+//            NSLog(@"pinyin: %@", ms);
+//        }
+//        if (CFStringTransform((__bridge CFMutableStringRef)ms, 0, kCFStringTransformStripDiacritics, NO)) {
+//            NSLog(@"pinyin: %@", ms);
+//        }  
+//    } 
+
+   NSLog(@"张三===%@", [self writePingyinWithHanzi:@"张三"]);
+}
+-(NSString*)writePingyinWithHanzi:(NSString*)hanzi
+{
+    if (!hanzi) {
+        return nil;
+    }
+    NSMutableString *string = [NSMutableString string];
+    for (int i = 0; i < [hanzi length]; i++){
+        unichar c = [hanzi characterAtIndex:0];
+        if (c >=0x4E00 && c <=0x9FFF){
+            [string appendFormat:@"%c",pinyinFirstLetter([hanzi characterAtIndex:i])];
+        }else{
+            [string appendFormat:@"%c",[hanzi characterAtIndex:i]];
+        }
+    }
+    return string;
+    
 }
 
 - (void)didReceiveMemoryWarning {
